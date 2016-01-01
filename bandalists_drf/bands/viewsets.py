3a7f78e675b8174a0bf2cd58a9376decfe5ca362
Stavros Kroustouris <kroustou@gmail.com>
2016-01-01 14:31:18 +0200

    add filters

--- bandalists_drf/bands/viewsets.py
+++ bandalists_drf/bands/viewsets.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from bands.serializers import BandSerializer
+from .filters import BandFilter
 
 
 class BandViewSet(viewsets.ModelViewSet):
     serializer_class = BandSerializer
+    filter_class = BandFilter
 
     def get_queryset(self):
         return self.request.user.band_set.all()
