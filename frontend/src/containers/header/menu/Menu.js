413596ea0cf657586c4d6e7cdf62a329d33a2412
Stavros Kroustouris <kroustou@gmail.com>
2016-09-08 20:42:16 +0300

    move logout on the right

--- frontend/src/containers/header/menu/Menu.js
+++ frontend/src/containers/header/menu/Menu.js
@@ -6,12 +6,12 @@ class Menu extends React.Component {
 	render() {
 		return (
 		    <ul className='seven columns menu' >
-		        { this.props.authenticated ? <li className='three columns'><Link activeClassName='active' to='/dashboard/'>dashboard</Link></li>: ''}
-				{ this.props.authenticated ? <li className='three columns'><Link activeClassName='active' to='/profile/'>profile</Link></li> : ''}
 				{ this.props.authenticated ? <li className='three columns'><a href="" onClick={e=>{
 					e.preventDefault()
 					this.props.logout()
 				}}>Logout</a></li>: ''}
+		        { this.props.authenticated ? <li className='three columns'><Link activeClassName='active' to='/dashboard/'>dashboard</Link></li>: ''}
+				{ this.props.authenticated ? <li className='three columns'><Link activeClassName='active' to='/profile/'>profile</Link></li> : ''}
 
 		        { this.props.authenticated ? ''	: <li className='three columns'><Link activeClassName='active' to='/login/'>login</Link></li> }
 		        <li className='three columns'><IndexLink to='/' activeClassName='active'>Home</IndexLink></li>
