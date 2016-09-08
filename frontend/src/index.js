dfb1fdbac61552390712dd040643b87ab3ff62f1
Kroustouris Stavros <kroustou@gmail.com>
2016-09-08 09:41:12 +0300

    css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,23 +2,17 @@ import {createStore} from "redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import {Router, Route, hashHistory} from "react-router";
-import App from "./components/App";
-import Posts from "./components/Posts";
-import Profile from "./components/Profile";
+import {Router, browserHistory} from "react-router";
 import {reducers} from  "./reducers";
+import {routes} from './routes';
+import './styles/style.scss';
 
 const store = createStore(reducers);
 
 // we need to render the app first...
 ReactDOM.render(
-     <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path="/" component={App}>
-            <Route path="profile" component={Profile}/>
-            <Route path="dashboard" component={Posts}/>
-          </Route>
-        </Router>
-      </Provider>,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes}/>
+    </Provider>,
     document.getElementById('app')
 );
