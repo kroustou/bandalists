8c28dec60d4b4e991f3e7fa153a52176f796ce9b
Stavros Kroustouris <kroustou@gmail.com>
2016-09-10 23:56:10 +0300

    add bandselector component

---
+++ frontend/src/containers/dashboard/BandSelector.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default (props) => {
+    return (
+        <ul className="bandFilter">
+        	<li>Band1</li>
+        </ul>
+    )
+}
--- frontend/src/containers/dashboard/index.js
+++ frontend/src/containers/dashboard/index.js
@@ -1,11 +1,13 @@
 import Posts from './Posts'
 import PostForm from '../../components/Dashboard/PostForm'
+import BandSelector from './BandSelector'
 import React from 'react'
 
 export const Dashboard = (props) => {
 
     return (
         <div className='dashboard'>
+        	<BandSelector/>
             <PostForm />
             <input onClick={props.getPosts} className='button-primary' value='get threads' type='button'/>
             <Posts {...props}/>
