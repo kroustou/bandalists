97e82895f4f031492447b5b6aeb3cfcb44aa532c
Kroustouris Stavros <kroustou@gmail.com>
2016-09-13 09:23:27 +0300

    show time in human format and comments styling

--- frontend/src/dashboard/components/containers/Thread.js
+++ frontend/src/dashboard/components/containers/Thread.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PostForm from '../PostForm'
+import moment from 'moment/moment'
 
 const Comment = ({thread}) =>  <Thread thread={thread} child={true}/>
 
 const Thread = ({thread, child}) => {
     const initialValues = {dashboard: thread.dashboard, parent: thread.id}
     return (
-        <div className="thread">
-          <span className="author">{thread.author}</span>
-          <span className="date">{thread.last_edit}</span>
+        <div className={child ? 'comment' : 'thread'}>
           <div className="text">{thread.text}</div>
+          <span className="author">{thread.author}</span> <span className="date">{moment.unix(thread.last_edit).fromNow()}</span>
           {thread.seen_by.length ? 'Seen by:': ''}
-          {!child ? <PostForm form={`postForm_${thread.id}`} initialValues={initialValues}/> : ''}
+          {!child ? <PostForm form={`postForm_${thread.id}`} initialValues={initialValues}/> : '' }
           {thread.children.map((thread) => {
             return <Comment key={thread.id} thread={thread}/>
           })}
