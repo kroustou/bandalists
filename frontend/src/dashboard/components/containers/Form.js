117fc72063a72791dba630acaa7f1e3af2cdbba7
Stavros Kroustouris <kroustou@gmail.com>
2016-09-25 17:48:30 +0300

    add markdown and open links in new tab

--- frontend/src/dashboard/components/containers/Form.js
+++ frontend/src/dashboard/components/containers/Form.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 const PostForm = (props) => {
-    const {handleSubmit, submitting, selectedBand, placeholder} = props
+    const {handleSubmit, submitting, selectedBand, placeholder, values} = props
 
     return (
         <form className="post-form" onSubmit={handleSubmit} autoComplete="off">
-            <div>
-                <div >
-                   <Field disabled={submitting} className="u-full-width" name="text" component='textarea'  placeholder={placeholder ? placeholder : 'Good news everyone at ' + selectedBand.name + '!'}/>
-                </div>
-            </div>
+            <Field disabled={submitting} className="u-full-width" name="text" component='textarea' onKeyDown={(e) => {
+                if (e.keyCode === 13 && e.shiftKey == false) {
+                    e.preventDefault()
+                    handleSubmit(values)
+                }}}  placeholder={placeholder ? placeholder : 'Good news everyone at ' + selectedBand.name + '!'}/>
         </form>
     )
 }
--- frontend/src/dashboard/components/containers/Thread.js
+++ frontend/src/dashboard/components/containers/Thread.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import PostForm from '../PostForm'
 import moment from 'moment/moment'
+import Remarkable from 'remarkable'
+import {escapeHtml, replaceEntities} from 'remarkable/lib/common/utils'
 
 const Comment = ({thread, deletePost, user}) =>  <Thread thread={thread} child={true} deletePost={deletePost} user={user}/>
 
-const Thread = ({thread, child, deletePost, user}) => {
-    const initialValues = {dashboard: thread.dashboard, parent: thread.id}
-    return (
-        <div className={child ? 'comment' : 'thread'}>
-            {user.username === thread.author ? <span className="delete" onClick={() => {deletePost(thread)}}>x</span>:''}
-            <div className="text">{thread.text}</div>
-            <span className="author">{thread.author}</span> <span className="date">{moment.unix(thread.last_edit).fromNow()}</span>
-            {thread.seen_by.length ? <div className="seen">{`Seen by: ${thread.seen_by.map(user => user)}`}</div>: ''}
-            {thread.children.map((thread) => {
-                return <Comment key={thread.id} thread={thread} deletePost={deletePost} user={user}/>
-            })}
-            {!child ? <PostForm form={`postForm_${thread.id}`} initialValues={initialValues} placeholder='Say something...' /> : '' }
-       </div>
-	)
+class Thread extends React.Component {
+    rawMarkup = () => {
+        let md = new Remarkable()
+        md.renderer.rules.link_open = function (tokens, idx /*, options, env */) {
+            var title = tokens[idx].title ? (' title="' + escapeHtml(replaceEntities(tokens[idx].title)) + '"') : '';
+            return '<a target="_blank" href="' + escapeHtml(tokens[idx].href) + '"' + title + '>';
+        }
+        let rawMarkup = md.render(this.props.thread.text)
+        return { __html: rawMarkup }
+    }
+
+    render = () => {
+        const {thread, child, deletePost, user} = this.props
+        const initialValues = {dashboard: thread.dashboard, parent: thread.id}
+        return (
+            <div className={child ? 'comment' : 'thread'}>
+                {user.username === thread.author ? <span className="delete" onClick={() => {deletePost(thread)}}>x</span>:''}
+                <div className="text" dangerouslySetInnerHTML={this.rawMarkup()}></div>
+                <span className="author">{thread.author}</span> <span className="date">{moment.unix(thread.last_edit).fromNow()}</span>
+                {thread.seen_by.length ? <div className="seen">{`Seen by: ${thread.seen_by.map(user => user)}`}</div>: ''}
+                {thread.children.map((thread) => {
+                    return <Comment key={thread.id} thread={thread} deletePost={deletePost} user={user}/>
+                })}
+                {!child ? <PostForm form={`postForm_${thread.id}`} initialValues={initialValues} placeholder='Say something...' /> : '' }
+           </div>
+    	)
+    }
 }
 
 export default Thread
