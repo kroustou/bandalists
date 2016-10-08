f5da1d911131c662df752764c865255ac9836413
Stavros Kroustouris <kroustou@gmail.com>
2016-10-08 14:47:56 +0300

    show avatar in notification

--- frontend/src/notifications/components/containers/Notification.js
+++ frontend/src/notifications/components/containers/Notification.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import moment from 'moment/moment'
-
+import Avatar from  '../../../profile/components/containers/Avatar'
 
 const RenderNotification = ({notification}) => {
     if (notification.notification_type === 'thread') {
         let message = JSON.parse(notification.message)
         return (
-            <div>
-                New {message.parent ? 'reply' : 'post'}  from {message.author.username}: {message.text}
+            <div className="row">
+                <div className="row">
+                    <div className="two columns">
+                        <Avatar img={message.author.avatar} />
+                    </div>
+                    <div className="ten columns">
+                        New {message.parent ? 'reply' : 'post'} from {message.author.username}
+                    </div>
+                </div>
+                <div className="row">{message.text}</div>
                 <span className="date">{moment.unix(message.last_edit).fromNow()}</span>
             </div>
         )
