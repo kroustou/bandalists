4ee4c6ede03a83cf6e5d540d72e5a25e7e0e3861
Stavros Kroustouris <kroustou@gmail.com>
2016-10-18 12:40:11 +0300

    style band selector menu

--- frontend/src/bands/components/containers/BandSelector.js
+++ frontend/src/bands/components/containers/BandSelector.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-const BandLabel = ({band}) => {
+const BandLabel = ({band, selected}) => {
     let bandImage = 'http://placehold.it/100x100/?text=' + band.name[0]
     if (band.bandimage_set) {
         const bandImageDict = band.bandimage_set.find(i => i.primary)
         if (bandImageDict) {
             bandImage = bandImageDict.avatar
-        } else {
         }
     }
     return (
-        <div className="band-label">
-            <span className="selected">{band.name}</span>
+        <div className={`band-label ${selected ? 'selected' : ''}`}>
+            <span>{band.name}</span>
             <div className='band-image'>
                 <img src={bandImage}/>
             </div>
@@ -23,7 +22,7 @@ export default ({bands, selectBand, selectedBand}) => {
 
     return (
         <div className="band-selector">
-            {selectedBand ? <BandLabel band={selectedBand}/> : <div className="message">Please select a band</div>}
+            {selectedBand ? <BandLabel band={selectedBand} selected={true}/> : <div className="message">Please select a band</div>}
             <ul>
                 { bands && bands.results ? bands.results.map(band => (
                     <li className={ selectedBand && band.slug === selectedBand.slug ? 'active' : ''} key={band.slug} onClick={() => selectBand(band) }><BandLabel band={band}/></li>
