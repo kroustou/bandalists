ce605d6e48d9e7b00efb20166224db6173ecce72
Stavros Kroustouris <kroustou@gmail.com>
2016-10-19 16:04:46 +0300

    fix lint errors

--- bandalists_drf/profiles/emails.py
+++ bandalists_drf/profiles/emails.py
@@ -1,9 +1,13 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 def send_invitation_email(instance):
     send_mail(
-        '%s has invited you to join %s at Bandalists!' % (instance.invited_by, instance.band.name),
+        '%s has invited you to join %s at Bandalists!' % (
+            instance.invited_by,
+            instance.band.name
+        ),
         'Hello, if you want to join %s, please click <a href="https://bandalists.com/sign-up/?token=%s">here</a>. Thank you.' % (instance.band.name, instance.token),
         settings.FROM_EMAIL,
         [instance.email],
