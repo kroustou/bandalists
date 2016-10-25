7e1dd62542de6722c48af790c3592765d62c42b7
Stavros Kroustouris <kroustou@gmail.com>
2016-10-25 14:53:55 +0300

    homepage

--- frontend/src/base/components/containers/Home.js
+++ frontend/src/base/components/containers/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default () => (
     <div id="home">
-        <h1>Bandalists Home Page</h1>
+        <h1>Get things done with Bandalists</h1>
+        <p>Bandalists is a platform which helps you and your bandmates keep things in order.</p>
     </div>
 )
